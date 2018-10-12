9453808b51dae4af24814e7dfcf330c777d8b70a
aleclivinghouse <aleclivinghouse@yahoo.com>
2018-10-12 13:48:30 -0700

    reducer finished

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,7 +1,7 @@
-import {FETCH_CHEESE_SUCCESS, FETCH_CHEESE_REQUEST, FETCH_CHEESE_ERROR} from '../actions/cheese';
+import {FETCH_CHEESES_SUCCESS, FETCH_CHEESE_REQUEST, FETCH_CHEESE_ERROR} from '../actions';
 
 const initialState = {
-  cheeses: ['test'],
+  cheeses: [],
   loading: false,
   error: null
 };
@@ -9,8 +9,8 @@ const initialState = {
 export const cheeseReducer = (state=initialState, action)=> {
   if(action.type === FETCH_CHEESE_REQUEST){
     return Object.assign({}, state, {loading: true})
-  } else if(action.type === FETCH_CHEESE_SUCCESS){
-    return Object.assign({}, state, {loading: false, error: null, cheese: action.cheeses})
+  } else if(action.type === FETCH_CHEESES_SUCCESS){
+    return Object.assign({}, state, {loading: false, error: null, cheeses: action.cheeses})
   } else if(action.type === FETCH_CHEESE_ERROR){
     return Object.assign({}, state, {loading: false, error: action.error})
   }
