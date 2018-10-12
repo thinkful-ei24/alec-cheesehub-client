cc08a7b1f04631be52cce88f1feee749276457a0
aleclivinghouse <aleclivinghouse@yahoo.com>
2018-10-12 13:47:02 -0700

    actions work

--- src/actions.js
+++ src/actions.js
@@ -1,27 +1,27 @@
-
+import {API_BASE_URL} from './config';
 export const fetchCheeses = () => (dispatch) => {
-  fetch('localhost:8080/api/cheeses')
+  fetch(`${API_BASE_URL}/api/cheeses`)
     .then(response => response.json())
     .then(cheeses => dispatch(fetchCheesesSuccess(cheeses)))
     .catch(error => console.log(error))
 }
 
 
-export const FETCH_CHEESE_REQUEST = 'FETCH_CHEESE_REQUEST'
+export const FETCH_CHEESE_REQUEST = 'FETCH_CHEESE_REQUEST';
 export const fetchCheeseRequest = ()=>{
     return {
         type: FETCH_CHEESE_REQUEST
     }
 }
-export const FETCH_CHEESE_ERROR = 'FETCH_CHEESE_ERROR'
+export const FETCH_CHEESE_ERROR = 'FETCH_CHEESE_ERROR';
 export const fetchCheeseError = (error)=>{
     return {
         type: FETCH_CHEESE_ERROR,
         error
     }
 }
-
-const fetchCheesesSuccess = (cheeses) => {
+export const FETCH_CHEESES_SUCCESS = 'FETCH_CHEESES_SUCCESS';
+export const fetchCheesesSuccess = (cheeses) => {
   return {
     type: 'FETCH_CHEESES_SUCCESS',
     cheeses
